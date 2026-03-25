76b39d31ca221086570b8a933d18c2c8c0c0fca2
killer whale <thekietvn4949@gmail.com>
2026-03-25 15:37:05 +0200

    code challenge 1

--- app/main.py
+++ app/main.py
@@ -1,11 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
+my_name = "Kiet"
+
 @app.get("/")
 def read_root():
-    return { "msg": "Testing!" }
+    return { "msg": f"testing" }
 
+@app.get("/hello")
+def read_root():
+    return { "msg": f"Hello {my_name}" }
+
+@app.get("/api/ip", response_class=HTMLResponse)
+def read_items(request: Request):
+    public_ip = request.client.host
+    return f"""
+    <html>
+        <body>
+            <h1>Your public IP is {public_ip}</h1>
+        </body>
+    </html>
+    """
+
+@app.get("/api/rawip")
+def read_items(request: Request):
+    public_raw_ip = request.client.host
+    return {"ip":f"{public_raw_ip}"}
 
 @app.get("/items/{id}")
 def read_item(item_id: int, q: str = None):
