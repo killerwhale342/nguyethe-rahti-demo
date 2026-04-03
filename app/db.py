2e51b5edaadada0fd255e41882a0c1bdf67dd87d
killer whale <thekietvn4949@gmail.com>
2026-04-03 20:04:28 +0300

    cc4

--- app/db.py
+++ app/db.py
@@ -16,4 +16,22 @@ def create_schema():
             --multi query need to be seperated by semi-colom
             --add columns
             ALTER TABLE rooms ADD COLUMN IF NOT EXISTS room_type VARCHAR;
+            ALTER TABLE rooms ADD COLUMN IF NOT EXISTS price NUMERIC(10, 2);
+            --code challenge 4
+            CREATE TABLE IF NOT EXISTS hotel_guests (
+                id SERIAL PRIMARY KEY,
+                guest_id INT NOT NULL,
+                first_name VARCHAR NOT NULL,
+                last_name VARCHAR NOT NULL,
+                address VARCHAR NOT NULL,
+                other_info VARCHAR
+            );
+            CREATE TABLE IF NOT EXISTS hotel_bookings (
+                id SERIAL PRIMARY KEY,
+                guest_id INT REFERENCES hotel_guests(id) NOT NULL,
+                room_id INT REFERENCES rooms(id) NOT NULL,
+                date_from DATE NOT NULL,
+                date_to DATE NOT NULL,
+                other_info VARCHAR
+            );
         """)
